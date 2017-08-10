67126e0cb27e8ac278d71d04f4455bf14f777aa9
HuangErGou <huangqinemail@163.com>
2017-08-10 21:21:32 +0800

    transpose.py 修改main 函数接口

--- transpose.py
+++ transpose.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 
 #
 # tf.transpose
-# 维度交换，eg: 2,3, 4 维变成 3, 2, 4 维
+# exchange dimentions, eg: 2, 3, 4 change to 3, 2, 4
 #
 
 dim1_size = 2
@@ -15,17 +15,18 @@ dim1 = 0
 dim2 = 1
 dim3 = 2
 
-raw = np.arange(dim1_size*dim2_size*dim3_size)
-raw = raw.reshape(dim1_size, dim2_size, dim3_size)
+if __name__ == "__main__":
+    raw = np.arange(dim1_size*dim2_size*dim3_size)
+    raw = raw.reshape(dim1_size, dim2_size, dim3_size)
 
-data = tf.transpose(raw, [dim2,dim1,dim3])
+    data = tf.transpose(raw, [dim2,dim1,dim3])
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    print("================================")
-    print(raw)
-    print(raw.shape)
-    print("================================")
-    print(sess.run(data))
-    print(data)
-    print("================================")
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        print("================================")
+        print(raw)
+        print(raw.shape)
+        print("================================")
+        print(sess.run(data))
+        print(data)
+        print("================================")
